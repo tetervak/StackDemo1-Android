b3440e08beda0a0c9e6c16ca10c6d8ce75cd8607
Alexander Tetervak <tetervak@sheridancollege.ca>
2022-12-04 20:16:01 -0500

    refactored the screen composable out

--- app/src/main/java/ca/tetervak/stackdemo/MainActivity.kt
+++ app/src/main/java/ca/tetervak/stackdemo/MainActivity.kt
@@ -3,41 +3,13 @@ package ca.tetervak.stackdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ca.tetervak.stackdemo.theme.StackDemoAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            StackDemoAndroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            StackDemoApp()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    StackDemoAndroidTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/ca/tetervak/stackdemo/StackDemoScreen.kt
@@ -0,0 +1,8 @@
+package ca.tetervak.stackdemo
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun StackDemoScreen() {
+    TODO("Not yet implemented")
+}
