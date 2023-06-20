38d948ab31a0ffa14d2cee9bdb64700193f49806
Alex Tetervak <tetervak@sheridancollege.ca>
2023-06-20 11:42:17 -0400

    added comments to StackDemo class

--- app/src/main/java/ca/tetervak/stackdemo/data/StackDemo.kt
+++ app/src/main/java/ca/tetervak/stackdemo/data/StackDemo.kt
@@ -3,12 +3,13 @@ package ca.tetervak.stackdemo.data
 import ca.tetervak.stackdemo.domain.StackItem
 
 class StackDemo {
-
+    // the first item of this list is the bottom of the stack
     private val list: MutableList<String> = mutableListOf()
 
     val itemCount: Int
         get() = list.size
 
+    // the first item of this list is the top of the stack
     val items: List<StackItem>
         get() = buildList(itemCount) {
             for ((index, value) in list.withIndex().reversed()) {
